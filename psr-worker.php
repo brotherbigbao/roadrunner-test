2e97f87e30c75eb1f76fab93aab3ffc19bb9afb0
liuyibao <734988964@qq.com>
2022-07-25 13:40:07 +0800

    psr-worker

--- psr-worker.php
+++ psr-worker.php
@@ -2,20 +2,29 @@
 use Spiral\RoadRunner;
 use Nyholm\Psr7;
 
-include 'vendor/autoload.php';
+include "vendor/autoload.php";
 
 $worker = RoadRunner\Worker::create();
 $psrFactory = new Psr7\Factory\Psr17Factory();
 
-$worker = new RoadRunner\Http\PSR7Worker($worker, $psrFactory, $psrFactory, $psrFactory);
+$psr7 = new RoadRunner\Http\PSR7Worker($worker, $psrFactory, $psrFactory, $psrFactory);
 
-while ($req = $worker->waitRequest()) {
+while (true) {
     try {
-        $rsp = new Psr7\Response();
-        $rsp->getBody()->write('Hello world!');
+        $request = $psr7->waitRequest();
 
-        $worker->respond($rsp);
-    } catch (\Throwable $e) {
-        $worker->getWorker()->error((string)$e);
+        if (!($request instanceof \Psr\Http\Message\ServerRequestInterface)) { // Termination request received
+            break;
+        }
+    } catch (\Throwable) {
+        $psr7->respond(new Psr7\Response(400)); // Bad Request
+        continue;
+    }
+
+    try {
+        // Application code logic
+        $psr7->respond(new Psr7\Response(200, [], 'Hello RoadRunner!'));
+    } catch (\Throwable) {
+        $psr7->respond(new Psr7\Response(500, [], 'Something Went Wrong!'));
     }
 }
